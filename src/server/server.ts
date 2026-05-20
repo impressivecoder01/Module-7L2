d981dbcfaa75b2e7c349830650dda55af8755139
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-21 03:41:50 +0600

    connect neon

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
 const app :Application = express()
 const port = 3000
 
 app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended: true}))
+
+const pool = new Pool({
+  connectionString: "postgresql://neondb_owner:npg_5pad6WVMHnDk@ep-soft-thunder-aq1l5zgs-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
 
 app.get('/', (req : Request, res: Response) => {
   res.status(200).json({
@@ -11,7 +18,12 @@ app.get('/', (req : Request, res: Response) => {
   })
 })
 app.post('/', async(req : Request, res: Response)=>{
-  console.log(req.body);
+  // console.log(req.body);
+  const {name, email, password} = req.body
+  res.status(201).json({
+    message : "created",
+    data: {name,email}
+  })
 })
 
 app.listen(port, () => {
