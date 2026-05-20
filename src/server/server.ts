6b30f57b688c090e03f15170aaaabb57a93197ed
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-21 02:36:20 +0600

    class two

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,17 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app :Application = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/', (req : Request, res: Response) => {
+  res.status(200).json({
+    "message" : "express server",
+    "author": "Nest Level"
+  })
+})
+app.post('/', async(req : Request, res: Response)=>{
+  console.log(req.body);
 })
 
 app.listen(port, () => {
