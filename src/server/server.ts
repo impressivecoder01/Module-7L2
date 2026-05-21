9630ced30fbb2e745fd1fc0a9a73884561117ee6
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-22 02:44:56 +0600

    databse connected

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,6 +11,30 @@ const pool = new Pool({
   connectionString: "postgresql://neondb_owner:npg_5pad6WVMHnDk@ep-soft-thunder-aq1l5zgs-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
 
+const initDB = async() =>{
+  try{
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(20),
+      email VARCHAR(20) NOT NULL,
+      password VARCHAR(20) NOT NULL,
+      is_active BOOLEAN DEFAULT true,
+      age INT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )
+      `)
+      console.log('DATABASE CONNECT');
+  }
+  catch(error){
+    console.log(error);
+  };
+  
+}
+  initDB()
+
+
 app.get('/', (req : Request, res: Response) => {
   res.status(200).json({
     "message" : "express server",
