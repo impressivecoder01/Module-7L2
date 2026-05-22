ca9303b62c5378382d7c0c370127eee0905bd6cd
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-23 01:26:57 +0600

    can not keep unupdated data

--- src/server/server.ts
+++ src/server/server.ts
@@ -129,8 +129,13 @@ app.put('/api/users/:id', async(req: Request, res: Response)=> {
     // console.log(result);
     try {
       const result = await pool.query(`
-    UPDATE users SET name = $1, password = $2, age = $3, is_active = $4
-    WHERE id = $5 RETURNING *
+    UPDATE users 
+    SET 
+    name = COALESCE($1,name), 
+    password = COALESCE($2,password), 
+    age = COALESCE($3,age), 
+    is_active = COALESCE($4,is_active)
+    WHERE id=$5 RETURNING *
     `, [name, password, age, is_active, id])
 
     if(result.rows.length === 0){
