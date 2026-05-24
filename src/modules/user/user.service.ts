b391a3f474c8b7af5726ad6ab8adee2b1a1fcc8b
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-25 02:50:52 +0600

    git  user  service

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,16 @@
+import { pool } from "../../db"
+
+const createUserIntoDB = async(payLoad: any) => {
+    const {name, email, password, age} = payLoad
+    const result = await pool.query(
+    `
+    INSERT INTO users (name, email, password, age) VALUES($1, $2, $3, $4)
+  RETURNING *
+    `, [name, email, password, age]
+  )
+  return result
+}
+
+export const userService = {
+    createUserIntoDB
+}
