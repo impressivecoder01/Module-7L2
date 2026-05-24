d13dc7454e44a04f7ced8ad33aef92faa6471b18
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-25 01:18:29 +0600

    pool and initDB moved in to db

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from "express"
-import {Pool} from "pg"
+// import {Pool} from "pg"
 import config from "../config"
+import { initDB, pool } from "../db"
 const app :Application = express()
 const port = config.port
 
@@ -8,31 +9,7 @@ app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended: true}))
 
-const pool = new Pool({
-  connectionString: config.connection_sting
-})
 
-const initDB = async() =>{
-  try{
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(20),
-      email VARCHAR(20) UNIQUE NOT NULL,
-      password VARCHAR(20) NOT NULL,
-      is_active BOOLEAN DEFAULT true,
-      age INT,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-      )
-      `)
-      console.log('DATABASE CONNECT successfully ');
-  }
-  catch(error){
-    console.log(error);
-  };
-  
-}
   initDB()
 
 
