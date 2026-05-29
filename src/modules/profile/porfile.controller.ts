070de0eee2ebef590b9416da2d5b7e715b6b8eaa
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-30 03:06:27 +0600

    error creating a profile

--- src/modules/profile/porfile.controller.ts
+++ src/modules/profile/porfile.controller.ts
@@ -4,6 +4,11 @@ import { profileService } from "./profile.service";
 const createProfile = async(req:Request, res: Response) => {
     try {
         const result = await profileService.createProfileIntoDB(req.body)
+        res.status(201).json({
+            success: true,
+            message: "Profile created successfully",
+            data: result.rows[0]
+        })
         
     } catch (error: any) {
         res.status(500).json({
