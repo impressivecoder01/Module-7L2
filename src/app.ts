2018239f0bdb570ed948e3e0b31fb20276c9c7c4
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-31 02:36:33 +0600

    middleware

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,27 @@ import config from "./config"
 import { userRoute } from "./modules/user/user.route"
 import { profileRoute } from "./modules/profile/profile.route"
 import { authRoute } from "./modules/auth/auth.route"
+import fs from "fs"
 // import { initDB, pool } from "./db"
 const app :Application = express()
 
 
 app.use(express.json())
 app.use(express.text())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+  console.log('Method - URL - Time:',req.method,req.url, Date.now());
+  const log = `\nMethod -> ${req.method} - Time ->${Date.now()} - URL -> ${req.url}\n`
+  fs.appendFile('logger.txt',log,(err)=> {
+    console.log(err);
+  })
+  next();
+});
 
 app.use('/api/users', userRoute)
 app.use('/api/profile', profileRoute);
-app.use('/api/auth',authRoute)
+app.use('/api/auth',authRoute);
+
 
 app.get('/', (req : Request, res: Response) => {
   res.status(200).json({
