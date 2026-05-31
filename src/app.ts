aa28a7e95b39ce09e3ab1d840852a61570b80e91
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-31 23:51:08 +0600

    moved logger middleware into logger.ts

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { userRoute } from "./modules/user/user.route"
 import { profileRoute } from "./modules/profile/profile.route"
 import { authRoute } from "./modules/auth/auth.route"
 import fs from "fs"
+import logger from "./middleware/logger"
 // import { initDB, pool } from "./db"
 const app :Application = express()
 
@@ -14,14 +15,7 @@ const app :Application = express()
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended: true}));
-app.use((req, res, next) => {
-  console.log('Method - URL - Time:',req.method,req.url, Date.now());
-  const log = `\nMethod -> ${req.method} - Time ->${Date.now()} - URL -> ${req.url}\n`
-  fs.appendFile('logger.txt',log,(err)=> {
-    console.log(err);
-  })
-  next();
-});
+app.use(logger);
 
 app.use('/api/users', userRoute)
 app.use('/api/profile', profileRoute);
