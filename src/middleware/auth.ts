50e6ec882457e3e0e3b9ff0c1bb70f8c26fe2ce2
impressivecoder1 <rohanrahman251@gmail.com>
2026-06-02 01:32:42 +0600

    decode jwt token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,22 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import config from "../config";
 
 const auth = ()=>{
     return async (req:Request, res:Response, next: NextFunction) => {
     // console.log('this is protected route');
-    console.log(req.headers.authorization );
+    // console.log(req.headers.authorization );
+    const token = req.headers.authorization;
+    // console.log(token);
+    if(!token){
+        res.status(401).json({
+            success: false,
+            message: "Unauthorized access",
+        })
+    }
+    const decode = jwt.verify(token as string,config.secret as string)
+    console.log(decode);
+
     next()
 }
 }
